15bdea190f1fa22edb66286887c44762210c5f7d
Uday Mittal <uday.mittal@niro.money>
2024-02-12 23:50:09 +0530

    Clerk, convex setup | Sidebar, Navbar and folder structure setup

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import { Sidebar } from './_components/sidebar';
+// import { Navbar } from "./_components/navbar";
+import { OrganizationSidebar } from './_components/organization-sidebar';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrganizationSidebar />
+          <div className="h-full flex-1">
+            {/* <Navbar /> */}
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayout;
