d87240ddbaafc23ae815b3cd71ded196e299d96b
Uday Mittal <uday.mittal@niro.money>
2024-03-02 19:08:15 +0530

    testing favorites

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -15,6 +15,8 @@ interface DashboardPageProps {
 const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   const { organization } = useOrganization();
 
+  console.log({ searchParams });
+
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
       {!organization ? (
